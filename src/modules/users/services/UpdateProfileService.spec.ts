c8860835620188a8841e420f3c6eb9f94bb27205
Henrique Balardin <balardinh@gmail.com>
2020-09-28 00:35:49 -0300

    feat: profile route and controller working it was created a route and controller for profile user, in this way, the user will be able to see your profile and also update it

--- src/modules/users/services/UpdateProfileService.spec.ts
+++ src/modules/users/services/UpdateProfileService.spec.ts
@@ -111,4 +111,14 @@ describe('UpdateProfile', () => {
       }),
     ).rejects.toBeInstanceOf(AppError);
   });
+
+  it('should not be able to update profile from non-existing user', async () => {
+    await expect(
+      updateProfile.execute({
+        user_id: 'non-existing-user-id',
+        name: 'John Born',
+        email: 'born@johncom',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
 });
