d5acb3d56e509084c1bbd3efb786998b64fcd462
Henrique Balardin <balardinh@gmail.com>
2020-09-29 11:04:12 -0300

    feat/fix: Days availables/files that wasn't added
    Fix: the last two commits does not added some files, so those files was
    added now.
    Feat: now, its possible to know when a day have appointments availables
    or not.

---
+++ src/modules/appointments/services/ListProvidersService.spec.ts
@@ -0,0 +1,38 @@
+import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
+import ListProvidersService from './ListProvidersService';
+
+let fakeUsersRepository: FakeUsersRepository;
+let listProviders: ListProvidersService;
+
+describe('ListProviders', () => {
+  beforeEach(() => {
+    fakeUsersRepository = new FakeUsersRepository();
+
+    listProviders = new ListProvidersService(fakeUsersRepository);
+  });
+
+  it('should be able to list the providers', async () => {
+    const user1 = await fakeUsersRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@fulano.com',
+      password: '123456',
+    });
+
+    const user2 = await fakeUsersRepository.create({
+      name: 'John Tre',
+      email: 'johntre@fulano.com',
+      password: '123456',
+    });
+    const loggedUser = await fakeUsersRepository.create({
+      name: 'John Qua',
+      email: 'johnqua@fulano.com',
+      password: '123456',
+    });
+
+    const users = await fakeUsersRepository.findAllProviders({
+      except_user_id: loggedUser.id,
+    });
+
+    expect(users).toEqual([user1, user2]);
+  });
+});
